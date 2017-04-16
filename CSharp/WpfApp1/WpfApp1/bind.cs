87c98a1ae605ddb43c03c13ffd29e396e24061c2
kurehajime <x.iidec+gh@googlemail.com>
2017-04-16 14:44:57 +0900

    TIL WPF

---
+++ CSharp/WpfApp1/WpfApp1/bind.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    class Bind : INotifyPropertyChanged
+
+    {
+        private string _Item1;
+        public string Item1 {
+            get {
+                return _Item1;
+            }
+            set {
+                _Item1 = value;
+                OnPropertyChanged("Item1");
+            }
+        }
+        private ICommand _Command1;
+        public ICommand Command1
+        {
+            get { return _Command1 = _Command1 ?? new Command1(this); }
+        }
+
+        private ICommand _Command2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged == null) return;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ICommand Command2
+        {
+            get { return _Command2 = _Command2 ?? new Command2(this); }
+        }
+    }
+
+
+    class Command1 : CommandBase
+    {
+        private Bind b;
+        public Command1(Bind b) {
+            this.b = b;
+        }
+        public override void Execute(object parameter) {
+            MessageBox.Show(b.Item1);
+        }
+    }
+
+    class Command2 : CommandBase
+    {
+        private Bind b;
+        public Command2(Bind b)
+        {
+            this.b = b;
+        }
+        public override void Execute(object parameter)
+        {
+            b.Item1="Hello";
+        }
+    }
+
+
+
+    public class CommandBase : ICommand
+    {
+        public virtual  bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public virtual void Execute(object parameter)
+        {
+           
+        }
+    }
+}
---
+++ CSharp/WpfApp1/WpfCustomControlLibrary1/CustomControl1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControlLibrary1
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+         
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary1"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary1;assembly=WpfCustomControlLibrary1"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : TextBox
+    {
+        TextBox textbox;
+
+        public string Value
+        {
+            get {
+                return (string)GetValue(ValueProperty);
+            }
+            set {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string), typeof(CustomControl1), new PropertyMetadata(""));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.textbox = this.GetTemplateChild("Textbox1") as TextBox;
+            if (textbox!=null) {
+                textbox.LostFocus += (sender,e)=>{
+                    TextBox tb = sender as TextBox;
+                    this.Value=tb.Text;
+                };
+            }
+        }
+        private static void ValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CustomControl1 cc = (CustomControl1)d;
+            //cc.Value = (string)e.NewValue;
+        }
+
+
+
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
