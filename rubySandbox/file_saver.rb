e8f9f3a01531eff513ac1a71a49e3ca4ba56a7b5
kurehajime <xiidec@gmail.com>
2021-09-04 23:39:47 +0900

    refa

--- rubySandbox/file_saver.rb
+++ rubySandbox/file_saver.rb
@@ -6,8 +6,14 @@ class NoGotcha < StandardError
 end
 
 class FileSaver
-  def run(&block)
-    state = State.new
+  attr_reader :file
+  def run(target_file, &block)
+    raise "TypeError" unless target_file.is_a?(TargetFile)
+
+    @file = target_file
+
+    state = State.new(@file)
+    state.save_name = @file.basename
     begin
       state.instance_eval(&block)
     rescue Gotcha=> e
@@ -28,10 +34,17 @@ class FileSaver
 end
 
 class State
-  attr_accessor :from_path,
-                :to_path,
-                :file_name
+  attr_accessor :to_path,
+                :save_name,
+                :empty,
+                :additional_filename,
+                :file
+  alias_method :empty?, :empty
 
+  def initialize(file)
+    @file = file
+    @empty = false
+  end
 
   def assert(bool)
     is bool
@@ -41,20 +54,68 @@ class State
     raise NoGotcha.new unless bool
   end
 
-  def to(dir_name,&block)
+  def dir(dir_name, &block)
     child = self.dup
     begin
       child.to_path = File.expand_path(dir_name,@to_path)
+
+      # 子stateで評価する
       child.instance_eval(&block)
+
+      # 子stateが保存対象外なら抜ける
+      raise NoGotcha if child.empty?
 
       # 子Stateで保存先が見つかった
       gotcha = Gotcha.new
       gotcha.path = child.to_path
-      gotcha.file_name = child.file_name
+      gotcha.file_name = child.save_name
       raise gotcha
     rescue NoGotcha
       # 子Stateで保存先が見つからなかった
     end
   end
 
+  def empty_dir()
+    @empty = true
+  end
+
+end
+
+require 'Pathname'
+
+class TargetFile
+  attr_accessor :dirname,
+                :full_path,
+                :basename,
+                :pure_basename,
+                :extname
+  attr_accessor :atime,
+                :ctime,
+                :size
+
+  def initialize(full_path,file_stat=nil)
+    path_name=Pathname.new(full_path)
+    @full_path = full_path
+    @dirname = dirname
+    @basename = path_name.basename(full_path).to_s
+    @pure_basename = path_name.basename(full_path).to_s.split(".")[0]
+    @extname = path_name.extname
+    if file_stat
+      @atime = file_stat.attime
+      @ctime = file_stat.ctime
+      @size = file_stat.size
+    end
+  end
+
+  def is_image?()
+    ["gif","jpg","jpeg","jpe","jfif","png","bmp","dib","rle","ico","ai","art","psd","tif","tiff","nsk","webp"].include?(@extname)
+  end
+
+  def is_sound?()
+    ["mp3","wma ","asf","3gp","3g2","aac","ogg","oga","mov","m4a","alac","flac","wav"].include?(@extname)
+  end
+
+  def is_movie?()
+    ["avi","flv","mpg","mpeg","mp4","mkv","mov","qt","wmv","asf","m2ts","ts","m4a","webm","ogm"].include?(@extname)
+  end
 end
--- rubySandbox/sandbox.rb
+++ rubySandbox/sandbox.rb
@@ -1,18 +1,31 @@
 require "./file_saver.rb"
 
 fs = FileSaver.new
-fs.run do
-  to "foo" do
+target_file = TargetFile.new("C:/temp/test.txt")
+fs.run(target_file) do
+  dir "foo" do
     assert 1+1==2
     assert 2+2==4
 
-    to "bar" do
+    dir "bar" do
       assert 1==2
     end
 
-    to "fizz" do
-      to "buzz" do
-        to "boon" do
+    dir "aiueo" do
+      assert 1==1
+      empty_dir
+
+      dir "kakikukeko" do
+        assert false
+      end
+    end
+
+    dir "fizz" do
+      dir "buzz" do
+        assert @file.basename.include?("test")
+
+        @save_name = "test2.txt"
+        dir "boon" do
           assert 1==0
         end
 
@@ -20,7 +33,7 @@ fs.run do
       end
     end
 
-    to "z" do
+    dir "z" do
       assert true
     end
   end
